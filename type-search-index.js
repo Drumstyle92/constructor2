4aef2bae9c9296f7c72a21d35f35856dc4be5a26
Dino <dino15031992@gmail.com>
2023-02-10 01:40:49 +0100

    commit

--- type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Student"},{"p":"<Unnamed>","l":"Teacher"},{"p":"<Unnamed>","l":"Test"}];updateSearchResults();
